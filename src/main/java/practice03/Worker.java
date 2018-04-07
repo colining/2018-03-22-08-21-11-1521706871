aab7b482650b03683bd62cf1ecb5052ee7315f0a
colining <18810782954@163.com>
2018-04-07 15:12:00 +0800

    complete practice 1~4

--- src/main/java/practice03/Worker.java
+++ src/main/java/practice03/Worker.java
@@ -1,4 +1,24 @@
 package practice03;
 
-public class Worker {
+public class Worker extends Person {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Worker(String name, int age) {
+        super(name, age);
+        this.name = name;
+        this.age = age;
+    }
+
+    public String introduce() {
+        return "I am a Worker. I have a job.";
+    }
 }
--- src/main/java/practice04/Worker.java
+++ src/main/java/practice04/Worker.java
@@ -1,4 +1,25 @@
 package practice04;
 
-public class Worker {
+
+public class Worker extends Person {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Worker(String name, int age) {
+        super(name, age);
+        this.name = name;
+        this.age = age;
+    }
+
+    public String introduce() {
+        return super.introduce() + " " + "I am a Worker. I have a job.";
+    }
 }
